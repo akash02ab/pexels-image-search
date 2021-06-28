013b8dea6b01534d5e09b94103aa0d077d7c0e3c
akash02ab <akash02.ab@gmail.com>
2021-06-28 09:51:48 +0530

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
 import { createClient } from "pexels";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const client = createClient("563492ad6f9170000100000113f525316273470d9df782d184f60f54");
 
 function App() {
 	const [data, setData] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const inputRef = useRef();
 
 	const fetchData = async () => {
 		let response = await client.photos.curated();
 		setData(response);
 		setLoading(false);
+	};
+
+	const changehandler = async () => {
+		if (inputRef.current.value === "") {
+			fetchData();
+			return;
+		}
+
+		const query = inputRef.current.value;
+		let response = await client.photos.search({ query, per_page: 15 });
+		setData(response);
 	};
 
 	useEffect(() => {
@@ -19,7 +31,10 @@ function App() {
 
 	return (
 		<div className="container">
-			<h1>Pexels Curated</h1>
+			<div className="head">
+				<h1>Pexels Curated</h1>
+				<input type="text" ref={inputRef} onChange={changehandler} />
+			</div>
 			{loading ? (
 				<h1>Loading . . .</h1>
 			) : (
